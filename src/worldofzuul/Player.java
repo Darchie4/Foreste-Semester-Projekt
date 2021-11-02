6bb861a14794d11dcf5e2aef26d2118071435f96
Mads Pedersen <mads456dk@gmali.com>
2021-11-02 12:54:26 +0100

    Started work on Inventory and Player classed
    Started work on Inventory and Player classed

--- src/worldofzuul/Player.java
+++ src/worldofzuul/Player.java
@@ -1,4 +1,12 @@
 package worldofzuul;
 
 public class Player {
+    private Inventory inventory;
+    private Item itemInHand;
+    private int energy;
+    private int points;
+
+    public Player() {
+        this.inventory = new Inventory();
+    }
 }
