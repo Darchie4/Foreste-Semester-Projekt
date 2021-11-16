9d6394bb5c7916cd4da3a87aea62f830f02652ea
Mads Pedersen <mads456dk@gmali.com>
2021-11-16 13:48:01 +0100

    Added support for eqiupment effect when picking up

--- src/worldofzuul/CommandWord.java
+++ src/worldofzuul/CommandWord.java
@@ -7,7 +7,7 @@ package worldofzuul;
 public enum CommandWord {
     GO("g\u00E5"), QUIT("afslut"), HELP("hj\u00E6lp"), UNKNOWN("?"), COLLECT("samle"),
     BUY("k\u00F8b"), USE("brug"), REMOVE("fjern"), RECYCLE("sorter"), UP("op"), DOWN("ned"),
-    RIGHT("h\u00F8jre"), LEFT("venstre"), PICKUP("samel");
+    RIGHT("h\u00F8jre"), LEFT("venstre"), PICKUP("indsamel");
     
     private String commandString;
     
