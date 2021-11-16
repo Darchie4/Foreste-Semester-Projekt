f6a948910054c60ac4208fca5edfd8379025ec53
Mads Pedersen <mads456dk@gmali.com>
2021-11-16 09:27:28 +0100

    Added movement thru commandwords
    Made it possible to move with commandwords

--- src/worldofzuul/CommandWord.java
+++ src/worldofzuul/CommandWord.java
@@ -6,7 +6,8 @@ package worldofzuul;
 * */
 public enum CommandWord {
     GO("g\u00E5"), QUIT("afslut"), HELP("hj\u00E6lp"), UNKNOWN("?"), COLLECT("samle"),
-    BUY("k\u00F8b"), USE("brug"), REMOVE("fjern"), RECYCLE("sorter");
+    BUY("k\u00F8b"), USE("brug"), REMOVE("fjern"), RECYCLE("sorter"), UP("op"), DOWN("ned"),
+    RIGHT("h\u00F8jre"), LEFT("venstre");
     
     private String commandString;
     
